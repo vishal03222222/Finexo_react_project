b91e81fd0b15ddcf30c7c822b29817b244ae4bf6
vishal <mgvishalctr@gmail.com>
2024-11-25 23:00:13 +0530

    done

---
+++ finexo_react/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css'
+import HeroArea from './component/Herosection';
+import ServiceSection from './component/Service';
+import AboutSection from './component/About';
+import WhyChooseUs from './component/Why';
+import TeamSection from './component/Team';
+import ClientSection from './component/Cllient';
+import InfoSection from './component/Info';
+import FooterSection from './component/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <HeroArea/>
+      <ServiceSection/>
+      <AboutSection/>
+      <WhyChooseUs/>
+      <TeamSection/>
+      <ClientSection/>
+      <InfoSection/>
+      <FooterSection/>
+    </div>
+  );
+}
+
+export default App;
